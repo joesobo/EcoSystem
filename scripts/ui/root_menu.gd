e961ef4536ba18e6e38ac3655bd17be5b8b67162
joesobo <soboleskijoe@gmail.com>
2024-06-05 17:52:18 -0700

    keep menus within viewport

--- scripts/ui/root_menu.gd
+++ scripts/ui/root_menu.gd
@@ -24,4 +24,25 @@ func _on_move_button_pressed(event):
 			else:
 				is_dragging = false
 	elif event is InputEventMouseMotion && is_dragging:
-		global_position += event.position - drag_offset
+		var new_position = global_position + (event.position - drag_offset)
+		ensure_within_viewport(new_position)
+
+func ensure_within_viewport(new_position: Vector2):
+	var viewport_rect = get_viewport_rect()
+
+	var move_button_global_pos = new_position + move_button.position
+
+	# Check horizontal boundaries
+	if move_button_global_pos.x < viewport_rect.position.x:
+		new_position.x = viewport_rect.position.x - move_button.position.x
+	elif move_button_global_pos.x + move_button.size.x > viewport_rect.position.x + viewport_rect.size.x:
+		new_position.x = viewport_rect.position.x + viewport_rect.size.x - move_button.size.x - move_button.position.x
+
+	# Check vertical boundaries
+	if move_button_global_pos.y < viewport_rect.position.y:
+		new_position.y = viewport_rect.position.y - move_button.position.y
+	elif move_button_global_pos.y + move_button.size.y > viewport_rect.position.y + viewport_rect.size.y:
+		new_position.y = viewport_rect.position.y + viewport_rect.size.y - move_button.size.y - move_button.position.y
+
+	# Update the global position with the corrected position
+	global_position = new_position
