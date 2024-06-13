b4db9ce2937be1699f6eb134d244c6ab07c4d6d8
joesobo <soboleskijoe@gmail.com>
2024-06-12 17:23:14 -0700

    pixel perfect outline shader

--- scripts/ui/menu_button.gd
+++ scripts/ui/menu_button.gd
@@ -4,12 +4,13 @@ var current_material
 
 func _ready():
 	current_material = material as ShaderMaterial
+	current_material.set_shader_parameter("color", Color(1,1,1,0))
 
 	connect("mouse_entered", Callable(self, "_on_mouse_entered"))
 	connect("mouse_exited", Callable(self, "_on_mouse_exited"))
 
 func _on_mouse_entered():
-	current_material.set_shader_parameter("outline_color", Color(1,1,1,1))
+	current_material.set_shader_parameter("color", Color(1,1,1,1))
 
 func _on_mouse_exited():
-	current_material.set_shader_parameter("outline_color", Color(1,1,1,0))
+	current_material.set_shader_parameter("color", Color(1,1,1,0))
