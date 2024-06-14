d4aaec2cd6193ab4190b79e9e45a718e0260221d
joesobo <soboleskijoe@gmail.com>
2024-06-14 13:09:06 -0700

    independent menu hover states

--- scripts/ui/menu_outline.gd
+++ scripts/ui/menu_outline.gd
@@ -12,16 +12,10 @@ func _ready():
 	UISingleton.connect("set_hovered_menu", Callable(self, "set_hovered"))
 	UISingleton.connect("clear_hovered_menu", Callable(self, "clear_hovered"))
 
-func set_hovered(_key):
-	current_material.set_shader_parameter("color", Color(1,1,1,1))
+func set_hovered(key):
+	if root_menu.menu.key == key:
+		current_material.set_shader_parameter("color", Color(1,1,1,1))
 
-func clear_hovered(_key):
-	current_material.set_shader_parameter("color", Color(1,1,1,0))
-
-# func set_focus(key):
-# 	if root_menu.menu.key == key:
-# 		current_material.set_shader_parameter("color", Color(1,1,1,1))
-
-# func clear_focus(key):
-# 	if root_menu.menu.key == key:
-# 		current_material.set_shader_parameter("color", Color(1,1,1,0))
+func clear_hovered(key):
+	if root_menu.menu.key == key:
+		current_material.set_shader_parameter("color", Color(1,1,1,0))
